047807d4a937187508db8a176c3c4c436b1c5694
Zac Downie <14842@holbertonstudents.com>
2026-09-01 15:19:28 +1000

    fix: memory leak in shell.c

--- tests/shell.c
+++ tests/shell.c
@@ -5,55 +5,52 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <string.h>
-
+/**
+ * main - running the main shell
+ *
+ * Return: 0 always
+ */
 int main(void)
 {
 	char *line;
-	int status;
+	int status = 0;
 	pid_t child;
-	pid_t my_pid;
 
-	status = 0;
-	my_pid = getpid();
 	/*signal(SIGINT, SIG_IGN);*/
 	while (status == 0)
 	{
 		line = readline();
-		if (strcmp(line,"") == 0)
+		if (strcmp(line, "") == 0)
 		{
 			perror("Error: no input");
 			free(line);
 		}
-		else if (strcmp(line,"exit") == 0)
+		else if (strcmp(line, "exit") == 0)
 		{
-			status = 1;
 			printf("Goodbye!\n");
+			free(line);
 			_exit(1);
 		}
-		else if(strcmp(line,"") > 0)
+		else if (strcmp(line, "") > 0)
 		{
 			child = fork();
-		  if (child == -1)
+			if (child == -1)
 			{
 				perror("Error:");
-				exit (-1);
+				exit(-1);
 			}
-		  if (child == 0)
+			if (child == 0)
 			{
-			
-				printf("child %u\n", child);
 				execprogram(line);
+				free(line);
 			}
 			else
 			{
-				printf("my_pid %u\n", my_pid);
 				wait(&status);
 			}
-
+			free(line);
 		}
 	}
-
-
 		return (0);
-	
+
 }
