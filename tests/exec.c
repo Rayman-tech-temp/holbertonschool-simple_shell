55b137ebeb7cb407f83f65e54541ad580b9c2c91
Zac Downie <14842@holbertonstudents.com>
2026-09-03 10:53:49 +1000

    feat: handle arguments & options

--- tests/exec.c
+++ tests/exec.c
@@ -1,43 +1,18 @@
 #include "main.h"
 
-
-char **split_string(int size, char *str)
-{
-    char *token;
-    char **array;
-    int i;
-    array = malloc(sizeof(str) * size);
-    if (array == NULL || size == 0)
-	{
-    return (NULL);
-	}
-    token = strtok(str, " ");
-    i = 0;
-    while (i < size - 1)
-    {
-        array[i] = token;
-        token = strtok (NULL, " ");
-        i++;
-    }
-    array[i] = NULL;
-
-    return (array);
-}
-
-
 /**
  * execprogram - execve example
  *
  * Return: Always 0.
  */
-int execprogram(char *command)
+int execprogram(char **command)
 {
-	char *argv[] = {"/bin/ls", "-l", "/usr", NULL};
+	/**char *argv[] = {"/bin/ls", "-l", "/usr", NULL};*/
 	
-    if (execve(argv[0], argv, NULL) == -1)
+    if (execve(command[0], command, NULL) == -1)
     {
         perror("Error:");
     }
-	execve(argv[0], argv, NULL);
+	execve(command[0], command, NULL);
     return (0);
 }
