1468528cfec15b3708fb6addc0f22c4243986372
Zac Downie <14842@holbertonstudents.com>
2026-09-03 11:06:06 +1000

    betty compliance

--- tests/exec.c
+++ tests/exec.c
@@ -2,17 +2,17 @@
 
 /**
  * execprogram - execve example
+ * @command: the arguments/code being executed
  *
  * Return: Always 0.
  */
 int execprogram(char **command)
 {
-	/**char *argv[] = {"/bin/ls", "-l", "/usr", NULL};*/
-	
-    if (execve(command[0], command, NULL) == -1)
-    {
-        perror("Error:");
-    }
+
+	if (execve(command[0], command, NULL) == -1)
+	{
+		perror("Error:");
+	}
 	execve(command[0], command, NULL);
-    return (0);
+	return (0);
 }
